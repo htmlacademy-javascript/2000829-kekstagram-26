0fe9ed313a4449407cf1cc7f75a0f164ba4f9c04
Evgeniya <osipova.dr17@yandex.ru>
2022-07-02 19:01:17 +1100

    Добавление функции, возвращающая случайное целое число

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/* eslint-disable no-console */
+function getRandomInt(min, max) {
+  if (min !== max && min >= 0 && 0 <= max && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return 'Не правильно введен диапазон!';
+}
+
+getRandomInt(2, 12);
+
+
 function validLenght(line, maxLeght) {
   if (line >= maxLeght) {
     return false;
