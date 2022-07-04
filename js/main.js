1f89db6d47e7c485a4324ed2eeb5dec777de4f6e
Evgeniya <osipova.dr17@yandex.ru>
2022-07-05 08:08:40 +1100

    Перенос констант в начало модуля

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/* eslint-disable no-console */
-function getRandomPositiveInteger (a, b) {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-
-function getRandomArrayElement (elements){
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
-}
-
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
-
-checkStringLength(25,140);
-
-
 const DESCRIPTIONS_PHOTOS =[
   'Котики на природе',
   'Кот и собака',
@@ -47,6 +28,24 @@ const USERS_NAMES = [
   'Max',
   'SayHi'
 ];
+
+/* eslint-disable no-console */
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomArrayElement (elements){
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+checkStringLength(25,140);
 
 const createUniqNumber = function() {
   let id = getRandomPositiveInteger(1, 25);
