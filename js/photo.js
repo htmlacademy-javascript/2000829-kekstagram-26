39a927e3a42633db3ddad927a72bc9c9f3e71d22
Evgeniya <osipova.dr17@yandex.ru>
2022-07-09 08:58:38 +1100

    Доработка кода comments

--- js/photo.js
+++ js/photo.js
@@ -6,10 +6,10 @@ const photos = allPhotos();
 
 const photoListFragment = document.createDocumentFragment();
 
-photos.forEach(({url, commets, likes}) => {
+photos.forEach(({url, comments, likes}) => {
   const photoElement = photoTemplate.cloneNode(true);
   photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__comments').textContent = commets;
+  photoElement.querySelector('.picture__comments').textContent = comments.length;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoListFragment.appendChild(photoElement);
 });
