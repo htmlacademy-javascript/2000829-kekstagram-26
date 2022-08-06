fed9e640ff283c7a0ee22fb5bafe9639d0133a16
Evgeniya <osipova.dr17@yandex.ru>
2022-08-06 17:56:41 +1100

    помощь другу

--- js/photo-effects.js
+++ js/photo-effects.js
@@ -2,13 +2,13 @@ const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const STEP_SCALE = 25;
 
-const BtnScaleSmaller = document.querySelector('.scale__control--smaller');
-const BtnScaleBigger = document.querySelector('.scale__control--bigger');
+const btnScaleSmaller = document.querySelector('.scale__control--smaller');
+const btnScaleBigger = document.querySelector('.scale__control--bigger');
 const valueScale = document.querySelector('.scale__control--value');
 const imgUploadPreview = document.querySelector('.img-upload__preview img');
 let originalScale = 100;
 
-BtnScaleSmaller.addEventListener('click', () => {
+btnScaleSmaller.addEventListener('click', () => {
   if (originalScale === MAX_SCALE || originalScale !== MIN_SCALE) {
     originalScale -= STEP_SCALE;
     valueScale.value = `${originalScale}%`;
@@ -16,7 +16,7 @@ BtnScaleSmaller.addEventListener('click', () => {
   }
 });
 
-BtnScaleBigger.addEventListener('click', () => {
+btnScaleBigger.addEventListener('click', () => {
   if (originalScale !== MAX_SCALE || originalScale === MIN_SCALE) {
     originalScale += STEP_SCALE;
     valueScale.value = `${originalScale}%`;
@@ -27,10 +27,11 @@ BtnScaleBigger.addEventListener('click', () => {
 
 //эффекты
 const sliderEffect = document.querySelector('.effect-level__slider');
-const effectsRadio = document.querySelectorAll('.effects__radio');
+//const effectsRadio = document.querySelectorAll('.effects__radio');
 const valueEffect = document.querySelector('.effect-level__value');
 const effectsList = document.querySelector('.effects__list');
-
+const uploadEffectLevel =  document.querySelector('.img-upload__effect-level');
+let currentEffect = '';
 
 noUiSlider.create(sliderEffect, { //пчм слайдер не появился на картинке?
   range: {
@@ -55,79 +56,95 @@ noUiSlider.create(sliderEffect, { //пчм слайдер не появился 
 
 sliderEffect.noUiSlider.on('update', () => {
   valueEffect.value = sliderEffect.noUiSlider.get();
-});
-
-effectsList.addEventListener('click', (evt) => { //причина здесь?
-
-  const effectRadio = evt.target.value; //причина здесь?
-
-  switch (effectRadio) { //причина здесь?
-
+  switch(currentEffect) {
     case 'chrome':
-      imgUploadPreview.classList.add('effects__preview--chrome');
-      sliderEffect.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
-        start: 0,
-        step: 0.1,
-      });
       imgUploadPreview.style.filter = `grayscale(${valueEffect.value})`;
       break;
     case 'sepia':
-      imgUploadPreview.classList.add('effects__preview--sepia');
+      imgUploadPreview.style.filter = `sepia(${valueEffect.value})`;
+      break;
+    case 'marvin':
+      imgUploadPreview.style.filter = `invert(${valueEffect.value}%)`;
+      break;
+    case 'phobos':
+      imgUploadPreview.style.filter = `blur(${valueEffect.value}px)`;
+      break;
+    case 'heat':
+      imgUploadPreview.style.filter = `brightness(${valueEffect.value})`;
+      break;
+    default:
+      imgUploadPreview.style.filter ='none';
+  }
+  //console.log(valueEffect.value);
+});
+uploadEffectLevel.classList.add('hidden');
+
+effectsList.addEventListener('change', (evt) => { //причина здесь?
+
+  const effectRadio = evt.target.value; //причина здесь?
+  currentEffect = effectRadio;
+  imgUploadPreview.className = '';
+  imgUploadPreview.classList.add(`effects__preview--${effectRadio}`);
+  uploadEffectLevel.classList.remove('hidden');
+  sliderEffect.removeAttribute('disabled');
+  switch (effectRadio) {
+
+    case 'chrome':
       sliderEffect.noUiSlider.updateOptions({
         range: {
           min: 0,
           max: 1,
         },
-        start: 0,
+        start: 1,
         step: 0.1,
       });
-      imgUploadPreview.style.filter = `sepia(${valueEffect.value})`;
+      break;
+    case 'sepia':
+      sliderEffect.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1,
+        },
+        start: 1,
+        step: 0.1,
+      });
       break;
     case 'marvin':
-      imgUploadPreview.classList.add('effects__preview--marvin');
       sliderEffect.noUiSlider.updateOptions({
         range: {
           min: 0,
           max: 100,
         },
-        start: 0,
+        start: 100,
         step: 1,
       });
-      imgUploadPreview.style.filter = `invert(${valueEffect.value}%)`;
       break;
     case 'phobos':
-      imgUploadPreview.classList.add('effects__preview--phobos');
       sliderEffect.noUiSlider.updateOptions({
         range: {
           min: 0,
           max: 3,
         },
-        start: 0,
+        start: 3,
         step: 0.1,
       });
-      imgUploadPreview.style.filter = `blur(${valueEffect.value}px)`;
       break;
     case 'heat':
-      imgUploadPreview.classList.add('effects__preview--heat');
       sliderEffect.noUiSlider.updateOptions({
         range: {
           min: 1,
           max: 3,
         },
-        start: 1,
+        start: 3,
         step: 0.1,
       });
-      imgUploadPreview.style.filter = `brightness(${valueEffect.value})`;
       break;
     default:
-      imgUploadPreview.classList.add('effects__preview--none');
-      imgUploadPreview.style.filter ='none'; // не уверена правильно Для эффекта «Оригинал» CSS-стили filter удаляются
-      sliderEffect.removeAttribute('disabled');
+      imgUploadPreview.style.filter = '';
+      uploadEffectLevel.classList.add('hidden');
+      sliderEffect.setAttribute('disabled', true);
   }
+  //console.log(imgUploadPreview);
 });
 
 
